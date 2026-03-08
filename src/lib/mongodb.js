f3cebb727e7737e88ef09427e300e13203589d0a
Pritesh23-P <pritesh0525@gmail.com>
2026-03-08 16:34:40 +0530

    update

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,50 +1,30 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
-import dns from 'dns/promises'
 
-async function getMongoClient() {
-  // Manually resolve SRV record using Google DNS
-  const resolver = new dns.Resolver()
-  resolver.setServers(['8.8.8.8', '8.8.4.4'])
+const uri = process.env.MONGODB_URI
 
-  const uri = process.env.MONGODB_URI
+if (!uri) {
+  throw new Error('Please define MONGODB_URI in .env.local')
+}
 
-  if (!uri) {
-    throw new Error('Please define MONGODB_URI in .env.local')
-  }
-
-  try {
-    // Test DNS resolution first
-    await resolver.resolve('cluster0.5herxdg.mongodb.net')
-  } catch (e) {
-    console.log('DNS test failed:', e.message)
-  }
-
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-    family: 4,
-    dnsResolve: (hostname, callback) => {
-      resolver.resolve4(hostname)
-        .then((addrs) => callback(null, addrs))
-        .catch((err) => callback(err))
-    },
-  })
-
-  return client.connect()
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 }
 
 let clientPromise
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
-    global._mongoClientPromise = getMongoClient()
+    const client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
   }
   clientPromise = global._mongoClientPromise
 } else {
-  clientPromise = getMongoClient()
+  const client = new MongoClient(uri, options)
+  clientPromise = client.connect()
 }
 
 export default clientPromise
